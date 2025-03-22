d16f6001669f238539adf549d66599ace62abe89
warsmill <warsmill@yandex.ru>
2025-03-22 14:38:45 +0300

    a program that determines, based on the month number of the year, what season the month belongs to

--- src/Main.java
+++ src/Main.java
@@ -43,5 +43,20 @@ public class Main {
             System.out.println("Доставки нет.");
         }
 
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case 1, 2, 12:
+                System.out.println("Зима");
+                break;
+            case 3, 4, 5:
+                System.out.println("Весна");
+                break;
+            case 6, 7, 8:
+                System.out.println("Лето");
+                break;
+            case 9, 10, 11:
+                System.out.println("Осень");
+                break;
+        }
     }
 }
