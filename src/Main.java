5d9877ee58bdc6977db4ca216afebe8dc7f10164
warsmill <warsmill@yandex.ru>
2025-03-22 14:19:59 +0300

    a program that determines whether a year is a leap year or not

--- src/Main.java
+++ src/Main.java
@@ -18,5 +18,13 @@ public class Main {
         }else if(clientOS == 1 && clientDeviceYear <= 2015){
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
+
+        int year = 2025;
+        if (year > 1584 && (year % 4 == 0 && (year % 100 != 0 || (year % 400 == 0 )))){
+            System.out.println(year + " год является високосным.");
+        }else {
+            System.out.println(year + " год не является високосныим.");
+        }
+
     }
 }
